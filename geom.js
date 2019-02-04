6f8ce9084a9515ce60019a5cdc21e303b011d714
Salem Alshehri <sal5m@hotmail.com>
2019-02-04 21:45:04 +0300

    Rectangle completed!!

--- geom.js
+++ geom.js
@@ -3,7 +3,29 @@ class Rectangle {
     this.length = length;
     this.width = width;
   }
+  get isSquare(){
+    if (this.height === this.width){
+    return true;
+    }
+    }
+    get calcArea(){
+    return this.height * this.width;
+    }
+    
+    get perimeter(){
+        return 2 * (this.height + this.width);
+    }
+    
 }
+
+const resultSquare = new Rectangle (4, 4);
+console.log(resultSquare.isSquare)
+
+const result = new Rectangle(3, 4);
+console.log(result.calcArea)
+
+const resultPer = new Rectangle(3, 4);
+console.log(resultPer.perimeter)
 
 
 class Triangle {
