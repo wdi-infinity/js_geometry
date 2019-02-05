f1077c93cce2bf13349ab280ccfdc3c945ef40f6
ASMAA-86 <asmabaabdullah@hotmail.com>
2019-02-05 08:47:53 +0300

    last update

--- geom.js
+++ geom.js
@@ -1,26 +1,73 @@
+/* Rectangle */
 class Rectangle {
   constructor(length, width) {
     this.length = length;
     this.width = width;
   }
+
+  isSquare(lengthNum, widthNum) {
+    if (lengthNum === widthNum)
+      //if ( this.length === this.width)
+      return true;
+  }
+
+  area(lengthNum, widthNum) {
+    let area = lengthNum * widthNum;
+    return area;
+  }
+
+  perimeter(lengthNum, widthNum) {
+    let perimeter = 2 * (lengthNum + widthNum);
+    return perimeter;
+  }
 }
 
-
+/* Triangle */
 class Triangle {
-  constructor(sideA, sideB, sideC){
+  constructor(sideA, sideB, sideC) {
     this.sideA = sideA;
     this.sideB = sideB;
     this.sideC = sideC;
   }
+
+  isEquilateral(first, second, third) {
+    if (first === second && first === third && second === third) {
+      return true;
+    }
+  }
+
+  isIsosceles(first, second, third) {
+    if (first === second || first === third || second === third) {
+      return true;
+    }
+  }
+  area(base, height) {
+    let area = (b * h) / 2;
+  }
+  isObtuse(angle) {
+    if (angle > 90) {
+      return true;
+    }
+  }
 }
 
-
+/* LineSegment */
 class LineSegment {
-  constructor(x1, y1, x2, y2){
+  constructor(x1, y1, x2, y2) {
     this.x1 = x1;
     this.y1 = y1;
     this.x2 = x2;
     this.y2 = y2;
+  }
+  lengthCalculator() {
+    let length1 = this.y1 - this.x1;
+    let length2 = this.y2 - this.x2;
+    console.log(
+      "The diffiranc between x1,y1" +
+        length1 +
+        "The diffiranc between x2,y2" +
+        length2
+    );
   }
 }
 
@@ -29,4 +76,4 @@ module.exports = {
   Rectangle: Rectangle,
   Triangle: Triangle,
   LineSegment: LineSegment
-}
+};
--- pull_request_template.md
+++ pull_request_template.md
@@ -1,14 +1,23 @@
 ### How difficult did you find this (0 - 10)? (0 very easy, 10 very difficult)
 
+7
 
 ### How long did it take?
 
+one hour/ for rivision and planning.
+one hour /for typing and testing, Although I need more than hour.
 
-### Was there anything that you struggled with?  If so, what?
+### Was there anything that you struggled with? If so, what?
 
+yes, how to use the properties inside the methods espicaly if I pass an arguments.
 
 ### Is there anything that you would like some further information on?
 
+I need more practise and time.
 
 ### Do you have any suggestions to improve this assignment?
 
+adding the math equation will make it much easier.
+
+Thank you ,
+Asmaa
